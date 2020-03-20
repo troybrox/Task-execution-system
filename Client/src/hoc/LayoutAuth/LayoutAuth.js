43a16c260bcd3276aef5212ea704a1dbf8049e45
OlegGavr <olej666117@gmail.com>
2020-03-21 02:39:41 +0400

    fix registration component and add some comments

--- Client/src/hoc/LayoutAuth/LayoutAuth.js
+++ Client/src/hoc/LayoutAuth/LayoutAuth.js
@@ -4,28 +4,34 @@ import {NavLink} from 'react-router-dom'
 import Auxiliary from '../Auxiliary/Auxiliary'
 
 class LayoutAuth extends React.Component {
+    submitHandler = event => {
+        event.preventDefault()
+    }
+
+    // Рендерим компонент аутентификации
+    // так же используем NavLink из библиотеки реакта для роутинга
     render() {
         return (
-        <Auxiliary>
-            <div className='header'>
-                <NavLink to={this.props.to} className='top_link'>
-                    <img src={this.props.img} />
-                    <span>{this.props.head}</span>
-                </NavLink>
-            </div>
-    
-            <div className="form_box">
-                <h2>{this.props.hTitle}</h2>
-                
-                <form>
-                    {this.props.children}
-                </form>
+            <Auxiliary>
+                <header className='header'>
+                    <NavLink to={this.props.to} className='top_link'>
+                        <img src={this.props.img} alt={this.props.hTitle}/>
+                        <span>{this.props.head}</span>
+                    </NavLink>
+                </header>
+            
+                <main className="form_box">
+                    <h2>{this.props.hTitle}</h2>
 
-                <NavLink to={this.props.to} className='link_registration'>
-                    {this.props.link}
-                </NavLink>
-            </div>
-        </Auxiliary>
+                    <form onSubmit={this.submitHandler}>
+                        {this.props.children}
+                    </form>
+
+                    <NavLink to={this.props.to} className='link_registration'>
+                        {this.props.link}
+                    </NavLink>
+                </main>
+            </Auxiliary>
         )
     }
 }
