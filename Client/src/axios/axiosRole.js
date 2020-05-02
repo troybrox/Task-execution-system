c1426c471b09521f9b8bd014eebac143c4c917d9
OlegGavr <olej666117@gmail.com>
2020-05-03 03:06:49 +0400

    fix axios 3.0

--- Client/src/axios/axiosRole.js
+++ Client/src/axios/axiosRole.js
@@ -1,7 +1,4 @@
 import axios from 'axios'
-import cookie from 'react-cookies'
-
-console.log(cookie.load('.AspNetCore.Application.Id'))
 
 // function getCookie(name) {
 //     let matches = document.cookie.match(new RegExp(
@@ -10,15 +7,9 @@ console.log(cookie.load('.AspNetCore.Application.Id'))
 //     return matches ? decodeURIComponent(matches[1]) : undefined;
 // }
 
-const api = axios.create({
-    baseURL: 'https://localhost:44303/'
+export default axios.create({
+    baseURL: 'https://localhost:44303/',
+    headers: {
+        Authorization:'Bearer '.concat(localStorage.getItem('token'))
+    }
 })
-api.interceptors.request.use(request => requestInterceptor(request))
-
-const requestInterceptor = (request) => {
-   request.headers['Authorization'] = 'Bearer '.concat(cookie.load('.AspNetCore.Application.Id'))
-   return request;
-}
-
-
-export default api
