745354e621db5d341d39fa82012a766052fed876
OlegGavr <olej666117@gmail.com>
2020-05-03 22:01:52 +0400

    fix axios

--- Client/src/axios/axiosRole.js
+++ Client/src/axios/axiosRole.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
 
-// function getCookie(name) {
-//     let matches = document.cookie.match(new RegExp(
-//       "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-//     ));
-//     return matches ? decodeURIComponent(matches[1]) : undefined;
-// }
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
 
 export default axios.create({
     baseURL: 'https://localhost:44303/',
     headers: {
-        Authorization:'Bearer '.concat(localStorage.getItem('token'))
+        Authorization:'Bearer '.concat(getCookie('.AspNetCore.Application.Id'))
     }
 })
