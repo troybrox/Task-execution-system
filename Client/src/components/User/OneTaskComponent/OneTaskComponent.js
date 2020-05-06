0f2ae01a17f00e1d95a940790d825a5d262a7ea2
OlegGavr <olej666117@gmail.com>
2020-05-07 01:56:22 +0400

    fix append

--- Client/src/components/User/OneTaskComponent/OneTaskComponent.js
+++ Client/src/components/User/OneTaskComponent/OneTaskComponent.js
@@ -71,7 +71,7 @@ class OneTaskComponent extends React.Component {
 
     onLoadFile = event => {
         const files = new FormData()
-        files.append(event.target.files[0])
+        files.append('file', event.target.files[0])
         this.setState({
             files
         })
